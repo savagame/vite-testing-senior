6c866417f1204382eba8b88e1c32b06a85c81602
roberto <goodhabitsamsam@gmail.com>
2025-09-21 16:24:05 +0800

    optimized github actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       reporter: ["text", "lcov"],
+      reportsDirectory: "./coverage",
       thresholds: {
         lines: 80,
         functions: 80,
